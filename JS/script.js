aaa88fa059b8fb3d726e28ab55e5fb0f3b7103f8
Foudil redaoui <df_redaoui@esi.dz>
2017-10-25 01:38:49 +0100

    BETA version

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,66 @@
-var colors = generateRandomColors(6);
+var numSquars = 6;
+var colors = generateRandomColors(numSquars);
 var pickedColor= pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.getElementById('message');
 var h1 = document.querySelector("h1");
+var btn = document.getElementById('reset');
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 colorDisplay.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
+easyBtn.addEventListener("click",function(){
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  numSquars=3;
+  colors = generateRandomColors(numSquars);
+  pickedColor=pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+  if (colors[i]) {
+    squares[i].style.background= colors[i];
+  } else {
+    squares[i].style.display= "none";
+  }
+
+  }
+})
+hardBtn.addEventListener("click",function(){
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquars=6;
+  colors = generateRandomColors(numSquars);
+  pickedColor=pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background= colors[i];
+    squares[i].style.display= "block";
+    }
+
+})
+
+  btn.addEventListener("click", function(){
+      colors = generateRandomColors(numSquars);
+      pickedColor= pickColor();
+      colorDisplay.textContent=pickedColor;
+      for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+      }
+      h1.style.background ="#009688";
+      btn.textContent="New Colors";
+})
+
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.background=colors[i];
   squares[i].addEventListener("click",function(){
     var clickedColor =this.style.background;
       if (clickedColor === pickedColor) {
         messageDisplay.textContent="Correct";
+        btn.textContent = "Play Again";
         changeColor(pickedColor);
         h1.style.background = clickedColor ;
     }else {
-      this.style.background= "#232323";
+      this.style.background= "#fafafa";
       messageDisplay.textContent="Try Again";
     }
   });
